58e39122e3581a0ede4546aeceead04f9b9f91d2
alyptik <alyptik@protonmail.com>
2017-10-05 19:06:08 -1000

    remove unneeded newlines

--- .zsh.d/plugins/ftc.zsh
+++ .zsh.d/plugins/ftc.zsh
@@ -1,63 +1,98 @@
-if echo $* | grep -q 'noprompt'
+# Print to stderr
+alias _cnf_print='echo -e 1>&2'
+
+# Parse options
+for opt in $*
+do
+    case $opt in
+        noprompt) cnf_noprompt=1 ;;
+        su) cnf_force_su=1 ;;
+        quiet) cnf_verbose=0 ;;
+        *) _cnf_print "find-the-command: unknown option: $opt"
+    esac
+done
+
+# Don't show pre-search warning if 'quiet' option is not set
+if [[ $cnf_verbose != 0 ]]
 then
-	command_not_found_handler(){
-		local CMD=$1
-		local PKGS=$(pacman -Foq /usr/bin/$CMD 2> /dev/null)
-		case $(echo $PKGS | wc -w) in
-			0) echo "$0: $CMD: command not found"
-				return 127 ;;
-			1) printf "\"$CMD\" may be found in package \"$PKGS\"\n" ;;
-			*)
-				local PKG
-				printf "\"$CMD\" may be found in the following packages:\n"
-				for PKG in `echo -n $PKGS`
-				do
-				printf "\t$PKG\n"
-				done
-		esac
-	}
+    _cnf_pre_search_warn(){
+        _cnf_print "find-the-command: \"$CMD\" is not found locally, searching in repositories..." 
+    }
+    _cnf_cmd_not_found(){
+        _cnf_print "find-the-command: command not found: $CMD"
+        return 127
+    }
 else
-	if [[ $EUID == 0 ]]
-	then _asroot(){ $*; }
-	else
-		if $* | grep -q 'su'
-		then _asroot() { su -c "$*"; }
-		else _asroot() { sudo $*; }
-		fi
-	fi
-	command_not_found_handler(){
-		local CMD=$1
-		local PKGS=$(pacman -Foq /usr/bin/$CMD 2> /dev/null)
-		case $(echo $PKGS | wc -w) in
-			0) return 127 ;;
-			1)
-				local ACT PS3="Action (0 to abort): "
-				local prompt_install(){
-					echo -n "Would you like to install this package? (y|n) "
-				read -q && (echo;_asroot pacman -S $PKGS) || (echo; return 127)
-				}
-				echo "\n\"$CMD\" may be found in package \"$PKGS\"\n"
-				echo "What would you like to do? "
-				select ACT in 'install' 'info' 'list files' 'list files (paged)'
-				do break
-				done
-				case $ACT in
-					install) _asroot pacman -S $PKGS ;;
-					info) pacman -Si $PKGS; prompt_install;;
-					'list files') pacman -Flq $PKGS; echo; prompt_install;;
-					'list files (paged)') [[ -z $PAGER ]] && local PAGER=less
-						pacman -Flq $PKGS | $PAGER
-						prompt_install ;;
-					*) echo; return 127
-				esac
-		;;
-			*)
-				local PKG PS3="$(printf "\nSelect a number of package to install (0 to abort): ")"
-				echo "\"$CMD\" may be found in the following packages:\n"
-				select PKG in `echo -n $PKGS`
-				do break
-				done
-				[[ -n $PKG ]] && _asroot pacman -S $PKG || return 127
-		esac
-	}
+    _cnf_pre_search_warn(){ : Do nothing; }
+    _cnf_cmd_not_found(){ return 127; }
 fi
+
+# Without installation prompt
+if [[ $cnf_noprompt == 1 ]]
+then
+    command_not_found_handler(){
+        local CMD=$1
+        _cnf_pre_search_warn
+        local PKGS=$(pacman -Foq /usr/bin/$CMD 2> /dev/null)
+        case $(echo $PKGS | wc -w) in
+            0) _cnf_cmd_not_found ;;
+            1) _cnf_print "\"$CMD\" may be found in package \"$PKGS\"" ;;
+            *)
+                local PKG
+                _cnf_print "\"$CMD\" may be found in the following packages:"
+                for PKG in `echo -n $PKGS`
+                do
+                _cnf_print "\t$PKG"
+                done
+        esac
+    }
+else
+# With installation prompt (default)
+    if [[ $EUID == 0 ]]
+    then _cnf_asroot(){ $*; }
+    else
+        if [[ $cnf_force_su == 1 ]]
+        then _cnf_asroot() { su -c "$*"; }
+        else _cnf_asroot() { sudo $*; }
+        fi
+    fi
+    command_not_found_handler(){
+        local CMD=$1
+        _cnf_pre_search_warn
+        local PKGS=$(pacman -Foq /usr/bin/$CMD 2> /dev/null)
+        case $(echo $PKGS | wc -w) in
+            0) _cnf_cmd_not_found ;;
+            1)
+                local ACT PS3="Action (0 to abort): "
+                local prompt_install(){
+                    _cnf_print -n "Would you like to install this package? (y|n) "
+                read -q && (_cnf_print;_cnf_asroot pacman -S $PKGS) || (_cnf_print; return 127)
+                }
+                _cnf_print "\n\"$CMD\" may be found in package \"$PKGS\"\n"
+                _cnf_print "What would you like to do? "
+                select ACT in 'install' 'info' 'list files' 'list files (paged)'
+                do break
+                done
+                case $ACT in
+                    install) _cnf_asroot pacman -S $PKGS ;;
+                    info) pacman -Si $PKGS; prompt_install;;
+                    'list files') pacman -Flq $PKGS; _cnf_print; prompt_install;;
+                    'list files (paged)') [[ -z $PAGER ]] && local PAGER=less
+                        pacman -Flq $PKGS | $PAGER
+                        prompt_install ;;
+                    *) _cnf_print; return 127
+                esac
+        ;;
+            *)
+                local PKG PS3="$(echo -en "\nSelect a number of package to install (0 to abort): ")"
+                _cnf_print "\"$CMD\" may be found in the following packages:\n"
+                select PKG in `echo -n $PKGS`
+                do break
+                done
+                [[ -n $PKG ]] && _cnf_asroot pacman -S $PKG || return 127
+        esac
+    }
+fi
+
+# Clean up environment
+unset opt cnf_force_su cnf_noprompt cnf_verbose
