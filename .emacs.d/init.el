26322170ef570bde38d7d901aec1b4d20385b2f4
alyptik <alyptik@protonmail.com>
2017-12-15 16:57:54 -1000

    .muttrc: update sidebar width

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,22 @@
-
 (setenv "ESHELL" (expand-file-name "~/bin/eshell"))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
+
+(require 'xclip)
+(turn-on-xclip)
+
+(require 'muse-mode)     ; load authoring mode
+(require 'muse-html)     ; load publishing styles I use
+(require 'muse-latex)
+(require 'muse-context)
+(require 'muse-project)  ; publish files in projects
+
+(add-hook 'c-mode-common-hook
+  (lambda ()
+    (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+      (ggtags-mode 1))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -11,17 +24,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (gruvbox)))
  '(custom-safe-themes
    (quote
     ("d29231b2550e0d30b7d0d7fc54a7fb2aa7f47d1b110ee625c1a56b30fea3be0f" default)))
  '(display-time-mode t)
+ '(font-use-system-font t)
  '(indicate-empty-lines t)
  '(package-selected-packages
    (quote
     (evil-search-highlight-persist evil-surround evil-commentary gruvbox-theme evil-terminal-cursor-changer evil-leader evil-easymotion evil-multiedit evil)))
- '(save-place t)
  '(save-place-mode t)
  '(show-paren-mode t)
  '(size-indication-mode t))
@@ -37,6 +49,5 @@
 (evil-mode 1)
 (global-undo-tree-mode)
 (unless (display-graphic-p)
-     (require 'evil-terminal-cursor-changer)
-     (evil-terminal-cursor-changer-activate) ; or (etcc-on)
-     )
+  (require 'evil-terminal-cursor-changer)
+  (evil-terminal-cursor-changer-activate)) ; or (etcc-on)
