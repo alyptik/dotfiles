82505c1e14d1cfc16a400b4037d3c4bb0997cb7b
Joey Pabalinas <joeypabalinas@gmail.com>
2017-12-28 15:47:58 -1000

    go.py: add go plugin

--- .zsh.d/plugins/screen.plugin.zsh
+++ .zsh.d/plugins/screen.plugin.zsh
@@ -8,7 +8,8 @@ if [[ $_GET_HOST == '' ]]; then
 fi
 
 # use the current user as the prefix of the current tab title
-TAB_TITLE_PREFIX='"`'$_GET_HOST'`:`'$_GET_PATH' | sed "s:..*/::"`$PROMPT_CHAR"'
+# TAB_TITLE_PREFIX='"`'$_GET_HOST'`:`'$_GET_PATH' | sed "s:..*/::"`$PROMPT_CHAR"'
+TAB_TITLE_PREFIX='"`'$_GET_PATH' | sed "s:..*/::"`$PROMPT_CHAR"'
 # when at the shell prompt, show a truncated version of the current path (with
 # standard ~ replacement) as the rest of the title.
 TAB_TITLE_PROMPT='$SHELL:t'
