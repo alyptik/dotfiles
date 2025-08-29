95cf4eb5e0b6adc849a8f4fbaaee81fa5ce5c1ad
Joey Pabalinas <joeypabalinas@gmail.com>
2025-08-29 09:42:52 -1000

    update mutt and neomutt configurations
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- bin/elfsyms.pl
+++ bin/elfsyms.pl
@@ -4,9 +4,10 @@ use strict;
 use warnings;
 
 print join(" ",
+	map { s|\((\s)|$1|gr; }
 	map { s|\s+| |gr; }
 	join(" ",
 	map { s|^.*\sT\s*(.*)$|$1\(| && $_ if /\bT\s[^_]/m; }
 	map { split /\n/, `nm -D $_`; }
 	keys %{{map { $_ => 1; }
-	map { s|^.*$|/lib/lib$&.so|r; } @ARGV}})) . "\n";
+	map { s|^.*$|/lib64/lib$&.so|r; } @ARGV}})) . "\n";
